2080e48dbf135f0c0c461cbd612a6194dcef8461
6londe <6londe@gmail.com>
2019-06-08 15:34:48 +0900

    Initial Commit

---
+++ ggumbang-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ggumbang-ios
+//
+//  Created by Changhoon Lee on 08/06/2019.
+//  Copyright © 2019 6londe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
